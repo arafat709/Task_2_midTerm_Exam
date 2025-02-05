14528e46b89ed8971b60eb645dbf116aacb539a3
arafat709 <mdarafat112007@gmail.com>
2025-02-05 21:32:28 +0600

    End

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-const carName = document.getElementById("Name");
-const modeName = document.getElementById("Model");
-const ColorName = document.getElementById("Color");
 const clickBtn = document.getElementById("addCartButton");
+let carData = document.querySelector(".car-data");
 
-clickBtn.addEventListener("click",(e)=>{
+clickBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    let name = carName.value;
-    let model = modeName.value;
-    let color = ColorName.value;
+    const carName = document.getElementById("name").value;
+    const modeName = document.getElementById("model").value;
+    const ColorName = document.getElementById("color").value;
 
-    let carsData = document.querySelectorAll("cars_data");
-    let newDiv = document.createElement("div");
+    let carNewName = document.createElement("h4");
+    let carNewModel = document.createElement("h4");
+    let carNewColor = document.createElement("h4");
+    let newIcon = document.createElement("i");
 
-})
+    newIcon.classList.add("fa-solid", "fa-xmark");
+
+    carNewName.innerText = carName
+    carNewModel.innerText = modeName
+    carNewColor.innerText = ColorName
+
+    let carList = document.createElement("div");
+
+    carList.appendChild(carNewName);
+    carList.appendChild(carNewModel);
+    carList.appendChild(carNewColor);
+    carList.appendChild(newIcon)
+
+    carList.classList.add("carListDiv")
+
+    carData.appendChild(carList);
+
+    newIcon.addEventListener("click", () => {
+        carList.style.display = "none"
+    })
+});
